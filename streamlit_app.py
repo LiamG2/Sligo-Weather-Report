1b737fb9ef2d6ae4a6e13af5644eff51f195856a
CADNurd <147918819+LiamG2@users.noreply.github.com>
2024-09-07 15:36:55 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from xmltodict import parse
 import requests
+import pandas as pd
 
 url = "https://www.met.ie/Open_Data/xml/county_forecast.xml"
 
@@ -8,4 +9,9 @@ data = requests.get(url).text
 
 parsed_data = parse(data)
 
-st.write(parsed_data)
+forecasts = parsed_data.get('forecast', {}).get('county', [])
+
+df = pd.json_normalize(forecasts)
+
+st.write("Weather Forecast Data")
+st.dataframe(df)
