08acd1ede8646c7f2b8fe9572507cd5ff8bec2ca
CADNurd <147918819+LiamG2@users.noreply.github.com>
2024-09-25 15:53:05 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,35 +26,18 @@ with st.sidebar:
 #   Relevant project set-up on Google Developers Console >> https://console.cloud.google.com/apis/credentials?project=opportune-geode-435020-g8
 #   Generated API Key from above project >> AIzaSyDpIvMkGb2WdHQC5xT1MHmefJZ7c3HRmlY
 
+
 gApiKey = gspread.api_key("AIzaSyDpIvMkGb2WdHQC5xT1MHmefJZ7c3HRmlY")
 
 public_sheet = gApiKey.open_by_url(
     'https://docs.google.com/spreadsheets/d/1pkysi4rP3zsl20GWUp_HFg3CRg44BXdaoJDI0fnqIHA/edit?usp=sharing')
 
-
-# # convert gspread output to plain string
-# today_Date = str((public_sheet.sheet1.get('A4')))
-# today_Wthr = str((public_sheet.sheet1.get('A5')))
-# tonight_Wthr = str((public_sheet.sheet1.get('A8')))
-# tomorrow_Wthr = str((public_sheet.sheet1.get('A11')))
-
-# # remove unnecessary chars [ ] ' from both string's ends
-# today_Date = today_Date.strip("[]'")
-# today_Wthr = today_Wthr.strip("[]'")
-# tonight_Wthr = tonight_Wthr.strip("[]'")
-# tomorrow_Wthr = tomorrow_Wthr.strip("[]'")
-
-# # remove substring '\n' from string - note the extra \ needed in \\n
-# # SEE >> https://stackoverflow.com/questions/42143302/how-can-i-remove-a-newline-character-in-a-string-in-python
-# today_Date = today_Date.replace('\\n', '')
-# today_Wthr = today_Wthr.replace('\\n', '')
-# tonight_Wthr = tonight_Wthr.replace('\\n', '')
-# tomorrow_Wthr = tomorrow_Wthr.replace('\\n', '')
-
-
 # String conversion/cleaning function
+#     convert gspread output to plain string
+#     remove unnecessary chars [ ] ' from both string's ends
+#     remove substring '\n' from string - note the extra \ needed in \\n
+#         SEE >> https://stackoverflow.com/questions/42143302/how-can-i-remove-a-newline-character-in-a-string-in-python
 def clean_gspread_output(data):
-    # Convert list-like output to string and remove unnecessary characters
     data = str(data).strip("[]'").replace('\\n', '')
     return data
 
@@ -64,7 +47,7 @@ today_Wthr = clean_gspread_output(public_sheet.sheet1.get('A5'))
 tonight_Wthr = clean_gspread_output(public_sheet.sheet1.get('A8'))
 tomorrow_Wthr = clean_gspread_output(public_sheet.sheet1.get('A11'))
 
-
+# Output to webpage/app
 st.title("Sligo Weather Report")
 st.write("_Today's weather - for both Sligo and the rest of Connacht_")
 st.write(" ")
